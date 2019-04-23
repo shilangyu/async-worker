e7e0113e98003950d96c18ce8cbfa97089eabda4
shilangyu <xmarcinmarcin@gmail.com>
2019-04-23 14:57:09 +0200

    removed web tests because web workers in node is not implemented

--- tests/task.test.ts
+++ tests/task.test.ts
@@ -1,10 +1,7 @@
-/**
- * @jest-environment {JEST-ENV}
- */
-import { task } from '{IMPORT-PATH}'
+import { task } from '../src/node'
 
-describe('{DESC} implementation of the task function', () => {
-	it('should return the computed value', async () => {
+describe('node implementation of the task function', () => {
+	it('should resolve the computed value', async () => {
 		const result = await task(() => {
 			let a = 5
 			a -= 1
@@ -16,5 +13,49 @@ describe('{DESC} implementation of the task function', () => {
 		})
 
 		expect(result).toBe(4)
+	})
+
+	it('should resolve the computed value with passed args', async () => {
+		const result = await task(d => {
+			let a = 5
+			a -= 1
+			const b = 12
+
+			var c = 1
+
+			return b / a + c - d
+		}, 5)
+
+		expect(result).toBe(-1)
+	})
+
+	it('should reject if passed parameter is not a function', async () => {
+		let wasError = false
+		await task(5 as any).catch(err => (wasError = true))
+
+		expect(wasError).toBeTruthy()
+	})
+
+	it('should reject if passed args are non-transferable', async () => {
+		let wasError = false
+		let res = await task(a => a(), () => 1).catch(err => (wasError = true))
+
+		expect(wasError).toBeTruthy()
+	})
+
+	it('should reject if it returns a non-transferable', async () => {
+		let wasError = false
+		await task(() => () => 1).catch(err => (wasError = true))
+
+		expect(wasError).toBeTruthy()
+	})
+
+	it('should reject if there was an internal error', async () => {
+		let wasError = false
+		await task(() => {
+			throw 'err'
+		}).catch(err => (wasError = true))
+
+		expect(wasError).toBeTruthy()
 	})
 })
