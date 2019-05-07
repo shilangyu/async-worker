1ee150982947fd2837075b278ac2ed1448e76910
shilangyu <xmarcinmarcin@gmail.com>
2019-05-07 19:09:07 +0200

    removed global evn files

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,4 +1,12 @@
 import * as fresh from './fresh'
 
+import { Worker } from 'worker_threads'
+import { randomBytes } from 'crypto'
+import * as globalMaker from '../globalMaker'
+import { BaseWorker, ENV } from '../BaseWorker'
+
+globalMaker.init(new BaseWorker(Worker, ENV.node), () => randomBytes(12).toString('base64'))
+
+export * from '../globalMaker'
+
 export { fresh }
-export * from './global'
--- src/web/index.ts
+++ src/web/index.ts
@@ -1,4 +1,17 @@
 import * as fresh from './fresh'
 
+import * as globalMaker from '../globalMaker'
+import { BaseWorker, ENV } from '../BaseWorker'
+
+globalMaker.init(
+	new BaseWorker(Worker, ENV.web),
+	() =>
+		'_' +
+		Math.random()
+			.toString(36)
+			.substr(2, 9)
+)
+
+export * from '../globalMaker'
+
 export { fresh }
-export * from './global'
