1ccc935d9183f794df694d071716580a86b84d7b
shilangyu <xmarcinmarcin@gmail.com>
2020-10-19 10:54:35 +0200

    migrated to webpack types

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,17 +1,15 @@
-import * as path from 'path'
+import { resolve } from 'path'
 import { Configuration } from 'webpack'
 
-const config: Configuration = {
+export default {
 	entry: './dist/index.js',
 	target: 'web',
 	output: {
 		filename: 'async-worker.web.js',
-		path: path.resolve(__dirname, 'dist'),
+		path: resolve(__dirname, 'dist'),
 		library: 'AsyncWorker',
 		libraryTarget: 'umd',
-		libraryExport: 'default'
+		libraryExport: 'default',
 	},
-	mode: 'production'
-}
-
-export default config
+	mode: 'production',
+} as Configuration
